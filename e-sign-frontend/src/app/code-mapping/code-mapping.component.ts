b0820f1e8a23a4ac694fc88fa1059867d75a2e89
narihanRSA <narihan.ellaithy@rsa.com>
2021-10-29 13:57:23 +0200

    Add file and manual code upload options

--- e-sign-frontend/src/app/code-mapping/code-mapping.component.ts
+++ e-sign-frontend/src/app/code-mapping/code-mapping.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit, EventEmitter } from '@angular/core';
-import { UploadFile, UploadInput, UploadOutput } from 'ng-uikit-pro-standard';
-import { humanizeBytes } from 'ng-uikit-pro-standard';
+import { Component } from '@angular/core';
 import { HttpEventType, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { UploadFilesService } from '../shared/_services/upload-file.service';
@@ -10,13 +8,6 @@ import { UploadFilesService } from '../shared/_services/upload-file.service';
   styleUrls: ['./code-mapping.component.css']
 })
 export class CodeMappingComponent {
-  // formData!: FormData;
-  // files: UploadFile[];
-  // uploadInput: EventEmitter<UploadInput>;
-  // humanizeBytes: Function;
-  // dragOver!: boolean;
-
-
   selectedFiles?: FileList;
   currentFile?: File;
   progress = 0;
@@ -71,51 +62,3 @@ export class CodeMappingComponent {
     }
   }
 }
-
-  // showFiles() {
-  //   let files = '';
-  //   for (let i = 0; i < this.files.length; i++) {
-  //     files += this.files[i].name;
-  //     if (!(this.files.length - 1 === i)) {
-  //       files += ',';
-  //     }
-  //   }
-  //   return files;
-  // }
-
-  // startUpload(): void {
-  //   const event: UploadInput = {
-  //     type: 'uploadAll',
-  //     url: '',
-  //     method: 'POST',
-  //     data: {  },
-  //   };
-  //   this.files = [];
-  //   this.uploadInput.emit(event);
-  // }
-
-  // cancelUpload(id: string): void {
-  //   this.uploadInput.emit({ type: 'cancel', id: id });
-  // }
-
-  // onUploadOutput(output: UploadOutput | any): void {
-
-  //   if (output.type === 'allAddedToQueue') {
-  //   } else if (output.type === 'addedToQueue') {
-  //     this.files.push(output.file); // add file to array when added
-  //   } else if (output.type === 'uploading') {
-  //     // update current data in files array for uploading file
-  //     const index = this.files.findIndex(file => file.id === output.file.id);
-  //     this.files[index] = output.file;
-  //   } else if (output.type === 'removed') {
-  //     // remove file from array when removed
-  //     this.files = this.files.filter((file: UploadFile) => file !== output.file);
-  //   } else if (output.type === 'dragOver') {
-  //     this.dragOver = true;
-  //   } else if (output.type === 'dragOut') {
-  //   } else if (output.type === 'drop') {
-  //     this.dragOver = false;
-  //   }
-  //   this.showFiles();
-  // }
-
